53e9d49af2b711bba3001e39e75eef84735682b5
Challice <ctrain149@gmail.com>
2022-10-19 17:18:43 -0400

    Setting up for building a plugin

---
+++ src/components.js
@@ -0,0 +1,5 @@
+import v2 from './v2.vue';
+import v3 from './v3.vue';
+
+
+export default { v2, v3 }
---
+++ src/v2.vue
@@ -0,0 +1,9 @@
+<script>
+  export default ({
+    name: 'VComponent',
+    props: { onRender: { default: () => {}, type: Function } },
+    render(h) {
+      return this.onRender(h);
+    }
+  });
+</script>
